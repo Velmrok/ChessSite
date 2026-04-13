21eaae97327962f4437a2ab0d96d25794a8fca45
Maciej Spławiński <splawis3@gmail.com>
2026-04-13 19:39:25 +0200

    create users controller with placeholder GetUsers
    Former-commit-id: 5915e316ef4293fa96bbb69b9c5d4b884a9b34b3

---
+++ backend/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+            
+        }
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            
+            return Ok(new { Name = "Jan" });
+        }
+
+    }
+}

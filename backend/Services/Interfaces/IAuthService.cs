82a92d53d7ced496a7425f0cebc2caa18f560e1f
Maciej Spławiński <splawis3@gmail.com>
2026-04-13 22:41:29 +0200

    add ErrorOr
    Former-commit-id: 62c3cc702ca36dedb4c7a2014849e8dc7d86e38e

--- backend/Services/Interfaces/IAuthService.cs
+++ backend/Services/Interfaces/IAuthService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.DTO.Auth;
+using backend.Models;
 
 namespace backend.Services.Interfaces
 {
     public interface IAuthService
     {
-        public Task RegisterAsync(RegisterRequest request);
+        public Task<List<User>> RegisterAsync(RegisterRequest request);
     }
 }

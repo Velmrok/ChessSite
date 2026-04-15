2230e19a114d5fec1d0ef139f2640e6941144739
Maciej Spławiński <splawis3@gmail.com>
2026-04-15 23:45:06 +0200

    add required attribute to Login prop
    Former-commit-id: 6ff0a37d280343becef4121fd87a35042ab1ee43

--- backend/DTO/Auth/LoginRequest.cs
+++ backend/DTO/Auth/LoginRequest.cs
@@ -7,6 +7,7 @@ using backend.CustomAttributes;
 [RequireOneOf("Login", "Email")]
 public class LoginRequest
 {
+    [Required]
     public string Login { get; set; } = string.Empty;
    
     [Required]

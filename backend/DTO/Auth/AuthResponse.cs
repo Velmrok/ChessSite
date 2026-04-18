5411e4a6ae67f007a49ba7043fb186fa514dcf86
Maciej Spławiński <splawis3@gmail.com>
2026-04-18 14:28:28 +0200

    delete unnecessary AuthResponse
    Former-commit-id: b7d2a968f9682acf2ba90f7e2300285b11322ac6

--- backend/DTO/Auth/AuthResponse.cs
+++
@@ -1,7 +0,0 @@
-
-
-using backend.DTO.Users;
-using backend.Models;
-
-namespace backend.DTO.Auth;
-public record AuthResponse(string AccessToken, string? RefreshToken = null, GetMeResponse? User = null);

74e2893af0abde57d0a2b8d3ecfd3440f02ca809
Maciej Spławiński <splawis3@gmail.com>
2026-04-18 17:27:09 +0200

    Add RefreshToken config
    Former-commit-id: 8c3cb86eace0201d0a9e47ee92280a753b13b9e5

---
+++ backend/Data/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,19 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace backend.Data.Configurations;
+
+public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+       
+        builder.HasIndex(rt => rt.Token).IsUnique();
+
+        builder.HasOne(rt => rt.User)
+               .WithMany()
+               .HasForeignKey(rt => rt.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ backend/Migrations/20260418151809_AddRefreshTokenIndex.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "RefreshTokens",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRevoked",
+                table: "RefreshTokens",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens",
+                column: "Token",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "IsRevoked",
+                table: "RefreshTokens");
+        }
+    }
+}

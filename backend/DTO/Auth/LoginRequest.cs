b60af860a8808ca0e42f92e5c87c7de07f2b3071
Maciej Spławiński <splawis3@gmail.com>
2026-04-18 12:38:09 +0200

    remove unnecessary attribute
    Former-commit-id: efabe031e49525d9b68f5802c2cd15ef5128b77d

--- backend/DTO/Auth/LoginRequest.cs
+++ backend/DTO/Auth/LoginRequest.cs
@@ -4,14 +4,14 @@ namespace backend.DTO.Auth;
 using backend.CustomAttributes;
 
 
-[RequireOneOf("Login", "Email")]
+
 public class LoginRequest
 {
     [Required(ErrorMessage = "requiredLogin")]
     public string Login { get; set; } = string.Empty;
    
     [Required(ErrorMessage = "requiredPassword")]
-    public required string Password { get; set; }
+    public string Password { get; set; }
  
 
 

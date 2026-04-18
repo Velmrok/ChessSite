fcdd9212ba3e9922265a99172cfe56b620938ab6
Maciej Spławiński <splawis3@gmail.com>
2026-04-18 12:24:26 +0200

    fix bug caused by static cookieOptions
    Former-commit-id: 2db6f1088e5b6fd649a57bf6c64ba1d153f7568a

--- backend/Services/Helpers/Auth/CookieService.cs
+++ backend/Services/Helpers/Auth/CookieService.cs
@@ -2,42 +2,41 @@ namespace backend.Services.Helpers.Auth
 {
     public static class CookieService
     {
-        static CookieOptions refreshTokenCookieOptions = new CookieOptions
-        {
-            HttpOnly = true,
-            Secure = false,
-            SameSite = SameSiteMode.Strict,
-            Expires = DateTime.UtcNow.AddDays(7)
-        };
-
-        static CookieOptions jwtCookieOptions = new CookieOptions
-        {
-            HttpOnly = true,
-            Secure = false,
-            SameSite = SameSiteMode.Strict,
-            Expires = DateTime.UtcNow.AddHours(1)
-        };
-
+      
         
         public static void SetJwtCookie(HttpResponse response, string token)
         {
+            CookieOptions jwtCookieOptions = new()
+            {
+                HttpOnly = true,
+                Secure = false,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTime.UtcNow.AddHours(1)
+            };
             response.Cookies.Append("accessToken", token, jwtCookieOptions);
         }
         public static void SetRefreshTokenCookie(HttpResponse response, string token)
         {
             if (string.IsNullOrEmpty(token))
                 return;
+            CookieOptions refreshTokenCookieOptions = new()
+            {
+                HttpOnly = true,
+                Secure = false,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTime.UtcNow.AddDays(7)
+            };
             response.Cookies.Append("refreshToken", token, refreshTokenCookieOptions);
  
         }
          public static void DeleteRefreshTokenCookie(HttpResponse response)
         {
-            response.Cookies.Delete("refreshToken", refreshTokenCookieOptions);
+            response.Cookies.Delete("refreshToken");
         }
         public static void DeleteJwtCookie(HttpResponse response)
         {
     
-            response.Cookies.Delete("accessToken", jwtCookieOptions);
+            response.Cookies.Delete("accessToken");
         }
        
     }

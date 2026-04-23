05b1f40d813e1a8c34b11f4aa522452309efc3ed
Maciej Spławiński <splawis3@gmail.com>
2026-04-23 20:36:03 +0200

    add separators between props

--- frontend/src/components/UserProfile/UserInfo.tsx
+++ frontend/src/components/UserProfile/UserInfo.tsx
@@ -8,6 +8,7 @@ type Props = {
     userInfo: UserInfo;
 };
 
+const separator = (<div className="col-span-2 h-[3px] bg-gradient-to-r from-transparent via-cyan-600 to-transparent "/>);
 export default function UserInfo({ className, userInfo }: Props) {
     const { t } = useTranslation("profile");
 
@@ -15,34 +16,43 @@ export default function UserInfo({ className, userInfo }: Props) {
     return (
         <div className={className || defaultClass}>
             <div className="grid grid-cols-2 gap-3 ml-4 text-white text-lg  font-MyFancyFont">
+
+
                 <div className="">{t('rating')}</div>
                 <div className="text-center">
                     <Rating className="flex flex-col items-center -translate-x-4" rating={userInfo.rating} />
                 </div>
+                {separator}
+
                 <div className="">{t('joinDate')}</div>
                 <div className="text-center">{userInfo?.createdAt}</div>
+                {separator}
+
                 <div className="">{t('gamesPlayed')}</div>
                 <div className="text-center">{userInfo?.gamesPlayed}</div>
+                {separator}
 
                 <div className="">{t('totalWins')}</div>
                 <div className="text-center">{userInfo?.totalWins}</div>
-
+                {separator}
+                
                 <div className="">{t('totalLosses')}</div>
                 <div className="text-center">{userInfo?.totalLosses}</div>
-
+                {separator}
                 <div className="">{t('totalDraws')}</div>
                 <div className="text-center">{userInfo?.totalDraws}</div>
-
+                {separator}
                 <div className="">{t('wins')}</div>
                 <div className="text-center ">
                     <Rating className="flex flex-col items-center -translate-x-4" rating={userInfo.wins} />
                 </div>
+                {separator}
 
                 <div className="">{t('losses')}</div>
                 <div className="text-center">
                     <Rating className="flex flex-col items-center -translate-x-4" rating={userInfo.losses} />
                 </div>
-
+                {separator}
                 <div className="">{t('draws')}</div>
                 <div className="text-center">
                     <Rating className="flex flex-col items-center -translate-x-4" rating={userInfo.draws} />

f7682ea002db635e229009895a3e20d30c28d5c9
Maciej Spławiński <splawis3@gmail.com>
2026-04-23 12:55:05 +0200

    Add OnSuccess callback

--- backend/Controllers/MyControllerBase.cs
+++ backend/Controllers/MyControllerBase.cs
@@ -8,16 +8,18 @@ namespace backend.Controllers;
 
 public class MyControllerBase : ControllerBase
 {
-   protected IActionResult? HandleError<T>(ErrorOr<T> result)
-{
-    if (!result.IsError) return null;
+    protected IActionResult HandleError<T>(ErrorOr<T> result, Func<T, IActionResult> onSuccess)
+    {
+        if (result.IsError)
+        {
+            var error = result.FirstError;
+            return Problem(
+                statusCode: error.ToStatusCode(),
+                title: error.Code,
+                detail: error.Description
+            );
+        }
 
-    var error = result.FirstError;
-
-    return Problem(
-        statusCode: error.ToStatusCode(),
-        title: error.Code,
-        detail: error.Description
-    );
-}
+        return onSuccess(result.Value);
+    }
 }

795acead0022e8945878f4e14fec6505af9799a2
Maciej Spławiński <splawis3@gmail.com>
2026-04-24 23:09:44 +0200

    remove old commented code

--- frontend/src/stores/useUserProfileStore.ts
+++ frontend/src/stores/useUserProfileStore.ts
@@ -1,26 +1,15 @@
-import { fetchUserFriends, fetchUserGameHistory, fetchUserProfile } from "@/services/userService";
 import type { PublicUser, UserProfile } from "@/types/user";
 import { create } from "zustand";
 
 type UserProfileStore = {
+
   profile: UserProfile | null;
   games: Array<ProfileGame>;
   friends: PublicUser[];
-
   gamesPage: number;
   friendsPage: number;
-
-  //loadingProfile: boolean;
-  //oadingGames: boolean;
-  // loadingFriends: boolean;
-
   totalFriendsPages: number;
   totalGamesPages: number;
-
-  // fetchProfile: (nickname: string) => Promise<void>;
-  //fetchGames: (nickname: string) => Promise<void>;
-  // fetchFriends: (nickname: string) => Promise<void>;
- 
 
   setGames: (games : Array<ProfileGame>) => void;
   setGamesPage: (page: number) => void;
@@ -36,57 +25,9 @@ export const useUserProfileStore = create<UserProfileStore>((set, get) => ({
 
   gamesPage: 1,
   friendsPage: 1,
-
-  //loadingProfile: false,
-  //loadingGames: false,
-  // loadingFriends: false,
-
   totalFriendsPages: 1,
   totalGamesPages: 1,
 
-  // async fetchProfile(nickname) {
-  //   const timeoutid = setTimeout(() => set({ loadingProfile: true }), 300);
-  //   try {
-  //     const profile = await fetchUserProfile(nickname);
-  //     set({ profile, loadingProfile: false });
-  //   } catch (error) {
-  //     console.error("Error fetching profile:", error);
-  //     set({ profile: null, loadingProfile: false });
-  //   } finally {
-  //       clearTimeout(timeoutid);
-  //   }
-  // },
-
-  // async fetchGames(nickname) {
-  //   const page = get().gamesPage;
-  //   const timeoutid = setTimeout(() => set({ loadingGames: true }), 300);
-  //   try {
-  //     const res = await fetchUserGameHistory(nickname, page);
-  //     set({ games: res.gameHistory, loadingGames: false, totalGamesPages: res.totalPages });
-  //   } catch (error) {
-  //     console.error("Error fetching games:", error);
-  //     set({ games: [], loadingGames: false, totalGamesPages: 1 });
-  //   } finally {
-  //     clearTimeout(timeoutid);
-  //   }
-  // },
-
-//   async fetchFriends(nickname) {
-//     const page = get().friendsPage;
-//     const timeoutid = setTimeout(() => set({ loadingFriends: true }), 300);
-//     try{
-//     const res = await fetchUserFriends(nickname, page);
-//     if(res.totalPages < get().friendsPage){
-//         set({ friendsPage: res.totalPages });
-//     }
-//     set({ friends: res.friendList, loadingFriends: false, totalFriendsPages: res.totalPages });
-//     } catch (error) {
-//       console.error("Error fetching friends:", error);
-//       set({ friends: [], loadingFriends: false, totalFriendsPages: 1 });
-//     }finally {
-//       clearTimeout(timeoutid);
-//     }
-// },
 setFriends(friends) {
     set({ friends });
   },

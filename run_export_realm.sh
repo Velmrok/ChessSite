2dd6025000042bcc40441ae6b92039b301cc95b4
Velmrok <splawis3@gmail.com>
2026-05-14 00:09:24 +0200

    fix exporting and keycloack conf

--- run_export_realm.sh
+++ run_export_realm.sh
@@ -4,6 +4,9 @@ CONTAINER_NAME="chess_keycloak_production"
 EXPORT_DIR="./.keycloak-export"
 EXPORT_FILE="chess-realm.json"
 
-docker exec "$CONTAINER_NAME" /opt/keycloak/bin/kc.sh export --file /tmp/"$EXPORT_FILE"
+docker exec "$CONTAINER_NAME" /opt/keycloak/bin/kc.sh export \
+    --dir .tmp/export \
+    --realm chess \
+    --users skip
 
 docker cp "$CONTAINER_NAME":/tmp/"$EXPORT_FILE" "$EXPORT_DIR"/"$EXPORT_FILE"
